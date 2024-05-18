cf51194dee91fe6e787050b566e95e0d43da2324
Big_Energy <andre90094@gmail.com>
2024-05-18 06:41:40 +0300

    removed config

--- src/main/java/com/bigenergy/lampicus/Lampicus.java
+++ src/main/java/com/bigenergy/lampicus/Lampicus.java
@@ -3,14 +3,11 @@ package com.bigenergy.lampicus;
 import com.bigenergy.lampicus.init.LampicusBlocks;
 import com.bigenergy.lampicus.init.LampicusItems;
 import com.bigenergy.lampicus.tab.LampicusCreativeTab;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -32,7 +29,7 @@ public class Lampicus {
         modEventBus.addListener(LampicusCreativeTab::registerTab);
 
         MinecraftForge.EVENT_BUS.register(this);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     @SubscribeEvent
